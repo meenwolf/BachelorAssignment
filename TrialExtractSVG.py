5bad95e8542bee8143fa262bbb72b84e6d0c0937
Marleen Siebons <meenwolfRZK@outlook.com>
2024-11-19 16:50:09 +0100

    Loaded the SVG file, now trying to extract the path id's

--- TrialExtractSVG.py
+++ TrialExtractSVG.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+import os
+from svgpathtools import svg2paths, svg2paths2, wsvg
+dir_path = os.path.dirname(os.path.realpath(__file__))
+PATH= os.path.abspath(os.path.join(dir_path, os.pardir))
+print(f"the path is {PATH}")
+PATH_drawings= PATH+"\\FloorplanAnnotated"
+print(f"the path to the drawings is {PATH_drawings}")
+paths, attributes, svg_attributes = svg2paths2(PATH_drawings+"\\CARRE 1412\\withPNG1412.3.svg", return_svg_attributes=True)
+print(f"paths: {paths} \n attributes: {attributes} \n svg attributes{svg_attributes}\n")
+for path in paths:
+    print(f"the id is: {path}")
+
