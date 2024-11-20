28fa05677d9f86a81607b50f34388e50d0d345d5
Marleen Siebons <meenwolfRZK@outlook.com>
2024-11-20 10:49:19 +0100

    saved the scale/length for one meter in a drawing

--- TrialExtractSVG.py
+++ TrialExtractSVG.py
@@ -6,8 +6,20 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 PATH= os.path.abspath(os.path.join(dir_path, os.pardir))
 PATH_drawings= PATH+"\\FloorplanAnnotated"
 #Get the paths and attributes of the third floor of Carré
+
+#IDEA: make a list of nodes, for all of the path starts and ends. After we also know the scale and made the
+#Edges have the correct real world weights, we can start at merging nodes in one, if they are closer than X meters in real life.
+
 paths, attributes = svg2paths(PATH_drawings+"\\CARRE 1412\\withPNG1412.3.svg")
-pprint(f"all paths are: {paths}")
+
+for path, attr in zip(paths, attributes):
+    if "inkscape:label" in attr.keys():
+        if attr['inkscape:label'] in {"CR31Mtr", "CR31mtr"}:
+            lengthForOneMeter= abs(path.start - path.end)
+            break
+
+print(f"length for one meter is {lengthForOneMeter}")
+
 for path, attr in zip(paths, attributes):
     if "inkscape:label" in attr.keys():
         print(f"inkscape:label is in attributes: {attr['inkscape:label']}")
