d9c61702ec837782afa1e129556488aa88a48433
Marleen Siebons <meenwolfRZK@outlook.com>
2024-11-20 10:25:29 +0100

    got the paths and the names from the svg

--- TrialExtractSVG.py
+++ TrialExtractSVG.py
@@ -1,12 +1,18 @@
 import os
 from svgpathtools import svg2paths, svg2paths2, wsvg
+from pprint import pprint
+# Get the path to the drawings
 dir_path = os.path.dirname(os.path.realpath(__file__))
 PATH= os.path.abspath(os.path.join(dir_path, os.pardir))
-print(f"the path is {PATH}")
 PATH_drawings= PATH+"\\FloorplanAnnotated"
-print(f"the path to the drawings is {PATH_drawings}")
-paths, attributes, svg_attributes = svg2paths2(PATH_drawings+"\\CARRE 1412\\withPNG1412.3.svg", return_svg_attributes=True)
-print(f"paths: {paths} \n attributes: {attributes} \n svg attributes{svg_attributes}\n")
-for path in paths:
-    print(f"the id is: {path}")
-
+#Get the paths and attributes of the third floor of Carré
+paths, attributes = svg2paths(PATH_drawings+"\\CARRE 1412\\withPNG1412.3.svg")
+pprint(f"all paths are: {paths}")
+for path, attr in zip(paths, attributes):
+    if "inkscape:label" in attr.keys():
+        print(f"inkscape:label is in attributes: {attr['inkscape:label']}")
+        print(f"and the path is: {path}")
+        start= path.start
+        end= path.end
+        edgeweight= abs(start-end)
+        print(f"edge has length: {edgeweight}")
