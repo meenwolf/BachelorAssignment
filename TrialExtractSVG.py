b7547b9782b2ae18af2c4e54e4d19a5f5a14b448
Marleen Siebons <meenwolfRZK@outlook.com>
2024-11-21 16:40:02 +0100

    changed the environment to conda so that i can use the academic licence for gurobi. Saved the data in a format the gurobi model needs its variable input to be and created a model and the first set of variables.

--- TrialExtractSVG.py
+++ TrialExtractSVG.py
@@ -1,6 +1,7 @@
 import os
 from svgpathtools import svg2paths, svg2paths2, wsvg
 from pprint import pprint
+from gurobipy import *
 # Get the path to the drawings
 dir_path = os.path.dirname(os.path.realpath(__file__))
 PATH= os.path.abspath(os.path.join(dir_path, os.pardir))
@@ -14,7 +15,8 @@ paths, attributes = svg2paths(PATH_drawings+"\\CARRE 1412\\withPNG1412.3.svg")
 
 for path, attr in zip(paths, attributes):
     if "inkscape:label" in attr.keys():
-        if attr['inkscape:label'] in {"CR31Mtr", "CR31mtr"}:
+        if attr['inkscape:label'] == "CR31mtr":
+            print(f"the scale label is: {attr['inkscape:label']}")
             lengthForOneMeter= abs(path.start - path.end)
             break
 
@@ -28,3 +30,57 @@ for path, attr in zip(paths, attributes):
         end= path.end
         edgeweight= abs(start-end)
         print(f"edge has length: {edgeweight}")
+
+# Gurobi uses dictionaries where the keys are pairs of (start, end) having a value
+# of the distance between them. To keep the information about the labels, I will create
+# a separate dictionary, having keys (start, end) and value the label name I assigned to it
+# then its easy to check if special actions are required.
+
+specialPaths={"CR":{3:{}}}
+hallways={}
+coordinateToNode={"CR":{3:{}}}
+nextnode=0
+for path, attr in zip(paths, attributes):
+    for i, line in enumerate(path):
+        start = line.start
+        end = line.end
+        edgeweight = abs(start - end)/lengthForOneMeter #edgeweight in meters
+        if "inkscape:label" in attr.keys():
+            if 'mtr' in attr['inkscape:label']:
+                continue
+            else:
+                specialPaths["CR"][3][(start, end)]=attr['inkscape:label']+str(i)
+
+
+        if not start in coordinateToNode["CR"][3]:
+            coordinateToNode["CR"][3][start]=nextnode
+            snode=nextnode
+            print(f" snode before increment: {snode}")
+            nextnode+=1
+            print(f" snode after increment: {snode}")
+        else:
+            snode=coordinateToNode["CR"][3][start]
+
+        if not end in coordinateToNode["CR"][3]:
+            coordinateToNode["CR"][3][end]=nextnode
+            enode=nextnode
+            print(f" enode before increment: {enode}")
+            nextnode+=1
+            print(f" enode after increment: {enode}")
+        else:
+            enode=coordinateToNode["CR"][3][end]
+        hallways[(snode, enode)] = edgeweight
+
+
+pprint(specialPaths)
+print("And all the hallways are:")
+pprint(hallways)
+
+# now try out the gurobi libary:
+m = Model()
+
+# Variables: the hallway connecting crossing i and j in the tour?
+vars = m.addVars(hallways.keys(), obj=hallways, vtype=GRB.BINARY, name='x')
+
+# Symmetric direction: use dict.update to alias variable with new key
+vars.update({(j,i):vars[i,j] for i,j in vars.keys()})
