79d140f411a100d51b201360a39591633c85ac23
Marleen Siebons <meenwolfRZK@outlook.com>
2025-01-09 16:56:47 +0100

    changed the name of the folder for the drawings of the solution, since it must be done in a result folder, and not the same head folder as the rest.

--- TestMoreFloors.py
+++ TestMoreFloors.py
@@ -402,7 +402,7 @@ def constructTrail(edges,vdum):
                 break
     return trail
 
-def drawEdgesInFloorplans(edges, nodeToCoordinate,elevatorEdges,specialEdges, figuresResultBuildings,folder, prefixfilename):
+def drawEdgesInFloorplans(edges, nodeToCoordinate,elevatorEdges,specialEdges, figuresResultBuildings,folderresult, prefixfilename):
     rainbowColors= getRainbowColors(len(edges))
     startedge= edges[0]
     endedge= edges[-1]
@@ -511,7 +511,7 @@ def drawEdgesInFloorplans(edges, nodeToCoordinate,elevatorEdges,specialEdges, fi
 
     # Draw the figures in a new file:
     for building, buildinginfo in figuresResultBuildings.items():
-        buildingResultPath= folder+f"\\{building}"
+        buildingResultPath= folderresult+f"\\{building}"
         for floor, floorinfo in buildinginfo.items():
             buildingName, buildingNumber = splitNameNumber(building)
             floortree= floorinfo['tree']
